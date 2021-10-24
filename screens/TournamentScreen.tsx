3ede8f5e07531fbf919c1cc02174c705291fbf12
Peter Robicheaux <probicheaux@gmail.com>
2021-10-24 12:23:47 -0700

    Have bracket paths rendering ish

--- screens/TournamentScreen.tsx
+++ screens/TournamentScreen.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { Text, View as BackgroundView, ViewProps } from "../components/Themed";
 import Colors from "../constants/Colors";
+import Svg, { Path, Polyline, Line, Circle } from "react-native-svg";
 
 import { RootStackScreenProps } from "../types";
 import {
@@ -16,60 +17,133 @@ import {
 
 import { tournamentPath, postRequest } from "../constants/Api";
 
+class DefaultDict {
+  constructor(defaultVal) {
+    return new Proxy(
+      {},
+      {
+        get: (target, name) => (name in target ? target[name] : defaultVal),
+      }
+    );
+  }
+}
+
 function View(props: ViewProps) {
   return (
     <BackgroundView darkColor={Colors["dark"]["touchableColor"]} {...props} />
   );
 }
 type TourneyProps = { winners: RoundProps[]; losers: RoundProps[] };
+function BracketLine() {
+  return (
+    <Svg style={{ width: 30, height: 30, backgroundColor: "transparent" }}>
+      <Line x1="0" y1="50%" x2="100" y2="50%" stroke="red" strokeWidth="1" />
+    </Svg>
+  );
+}
+function Junction({ isLineConnector }: { isLineConnector: boolean }) {
+  if (!isLineConnector) {
+    return <BracketLine />;
+  }
+  let total = 100 + 2 * VerticalMargin;
+  let start = 0; //100 * (25 / total);
+  let end = 100; //(100 * (total - 25)) / total;
+  return (
+    <Svg style={{ width: 30, height: 120, backgroundColor: "transparent" }}>
+      <Line
+        x1="0"
+        y1={start.toString() + "%"}
+        x2="0"
+        y2={end.toString() + "%"}
+        stroke="red"
+        strokeWidth="2"
+      />
+      <Line x1="0" y1="50%" x2="100" y2="50%" stroke="red" strokeWidth="1" />
+    </Svg>
+  );
+}
+
+const VerticalMargin = 5;
 const CustomSeed = ({
   seed,
   breakpoint,
   roundIndex,
   seedIndex,
   tourney,
+  positionInfo,
+  setPositionInfo,
 }: {
   seed: RenderSeedProps["seed"];
   breakpoint: RenderSeedProps["breakpoint"];
   roundIndex: RenderSeedProps["roundIndex"];
   seedIndex: RenderSeedProps["seedIndex"];
   tourney: TourneyProps;
+  positionInfo: any;
+  setPositionInfo: Function;
 }) => {
   // ------ assuming rounds is the losers brackets rounds ------
   // losers rounds usually got some identical seeds amount like (2 - 2 - 1 - 1)
 
   // mobileBreakpoint is required to be passed down to a seed
-  let rounds = tourney[seed.losers ? "losers" : "winners"];
+  const bracket = seed.losers ? "losers" : "winners";
+  let rounds = tourney[bracket];
 
   const isLineConnector: boolean =
     rounds[roundIndex].seeds.length === rounds[roundIndex + 1]?.seeds.length;
 
   //const Wrapper = isLineConnector ? SingleLineSeed : Seed;
+  var [x, y, width, height] = positionInfo[bracket][roundIndex];
+
+  function LineComp() {
+    if (roundIndex === 0) {
+      return <View />;
+    } else {
+      return <Junction isLineConnector={isLineConnector} />;
+    }
+  }
+  function EndComp() {
+    if (roundIndex === rounds.length - 1) {
+      return <View />;
+    } else {
+      return <BracketLine />;
+    }
+  }
   return (
-    <BackgroundView
-      style={isLineConnector ? styles.singleLineSeed : styles.seed}
-    >
-      <View style={styles.seedItem}>
-        <View>
-          <View style={styles.seedTeam}>
-            <Text style={styles.seedText}>
-              {seed.teams[0]?.name || "NO TEAM "}
-            </Text>
-            <Text style={styles.scoreText}>{seed.teams[0]?.game}</Text>
-          </View>
-          <View
-            style={styles.separator}
-            lightColor="#eee"
-            darkColor="rgba(255,255,255,0.1)"
-          />
-          <View style={styles.seedTeam}>
-            <Text style={styles.seedText}>
-              {seed.teams[1]?.name || "NO TEAM "}
-            </Text>
-            <Text style={styles.scoreText}>{seed.teams[1]?.game}</Text>
+    <BackgroundView style={{ flexDirection: "row", alignItems: "center" }}>
+      <LineComp />
+      <BackgroundView
+        style={styles.seed}
+        onLayout={(event) => {
+          let { x, y, width, height } = event.nativeEvent.layout;
+          positionInfo[bracket][roundIndex] = [x, y, width, height];
+          setPositionInfo(positionInfo);
+          console.log("POSITION INFO");
+          console.log(JSON.stringify(positionInfo));
+        }}
+      >
+        <View style={styles.seedItem}>
+          <View>
+            <View style={styles.seedTeam}>
+              <Text style={styles.seedText}>
+                {seed.teams[0]?.name || "NO TEAM "}
+              </Text>
+              <Text style={styles.scoreText}>{seed.teams[0]?.game}</Text>
+            </View>
+            <View
+              style={styles.separator}
+              lightColor="#eee"
+              darkColor="rgba(255,255,255,0.1)"
+            />
+            <View style={styles.seedTeam}>
+              <Text style={styles.seedText}>
+                {seed.teams[1]?.name || "NO TEAM "}
+              </Text>
+              <Text style={styles.scoreText}>{seed.teams[1]?.game}</Text>
+            </View>
           </View>
         </View>
-      </View>
+      </BackgroundView>
+      <EndComp />
     </BackgroundView>
   );
 };
@@ -104,6 +178,10 @@ export default function TournamentScreen({
     tourney_id: 69420,
   });
 
+  const [positionInfo, setPositionInfo] = useState({
+    winners: new DefaultDict([0, 0, 0, 0]),
+    losers: new DefaultDict([0, 0, 0, 0]),
+  });
   function seedRenderer({
     seed,
     breakpoint,
@@ -121,6 +199,8 @@ export default function TournamentScreen({
       roundIndex,
       seedIndex,
       tourney: tourney,
+      positionInfo: positionInfo,
+      setPositionInfo: setPositionInfo,
     });
   }
   return (
@@ -179,7 +259,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     position: "relative",
     flexDirection: "column",
-    margin: 2,
+    margin: "5%",
   },
   seedTeam: {
     padding: "1.5rem 1.5rem",
@@ -188,7 +268,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     marginHorizontal: "10px",
-    marginVertical: 5,
+    marginVertical: "5%",
   },
   seed: {
     padding: "1em 1.5em",
@@ -201,21 +281,6 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     fontSize: 14,
-    margin: "5px",
-    marginHorizontal: "40px",
-  },
-  singleLineSeed: {
-    padding: "1em 1.5em",
-    minWidth: 225,
-    width: "100%",
-    position: "relative",
-    display: "flex",
-    alignItems: "center",
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    fontSize: 14,
-    margin: "1px",
   },
   separator: {
     marginVertical: 3,
