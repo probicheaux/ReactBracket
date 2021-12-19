33c4377b0a55f27e18d254be924cd3ef59cdd5df
William <william.bertrand94@gmail.com>
2021-12-18 18:24:24 -0700

    small changes to ui for login/register

--- components/AppContext.tsx
+++ components/AppContext.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+type ColorScheme = 'light' | 'dark'
+
 const AppContext = React.createContext({
   token: "" as string | null | undefined,
-  scheme: "",
+  scheme: 'light' as ColorScheme,
 });
 
 export default AppContext;
