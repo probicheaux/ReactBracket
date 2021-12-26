d01629a24ca07670ae5f60de6035835e6727e297
Peter Robicheaux <probicheaux@gmail.com>
2021-12-25 23:24:12 -0500

    Oops format on save did a lot

--- api/Requests.ts
+++ api/Requests.ts
@@ -3,54 +3,69 @@ import { apiHost, apiUnversionedHost } from "../constants/Api";
 import { Tournament, Bracket } from "../models";
 import { getRequest, postRequest } from "./BaseRequests";
 
-
 type LoginResponse = {
-    token: string
-}
-
+  token: string;
+};
 
 // Authentication Requests
 // TODO: Replace with direct to firebase funcs
 export const loginUser = async (username: string, password: string) => {
-    const resp = await postRequest(`${apiUnversionedHost}/user/login`, { username, password });
-    return (resp).token;
-}
+  const resp = await postRequest(`${apiUnversionedHost}/user/login`, {
+    username,
+    password,
+  });
+  return resp.token;
+};
 
-export const registerUser = async (email: string, username: string, password: string) => {
-    const resp = await postRequest(`${apiUnversionedHost}/user/register-from-email/`, { email, username, password });
-    return resp.data
-}
+export const registerUser = async (
+  email: string,
+  username: string,
+  password: string
+) => {
+  const resp = await postRequest(
+    `${apiUnversionedHost}/user/register-from-email/`,
+    { email, username, password }
+  );
+  return resp.data;
+};
 
 // Tournament Requests ---------------
 
 type GetTournamentsResponse = {
-    tournaments: Tournament[]
-}
+  tournaments: Tournament[];
+};
 
 // Get all user tournaments
 export const getTournaments = async (user: User) => {
-    const url = `${apiHost}/tournaments`;
-    const resp = await getRequest(url, user);
-    return (resp as GetTournamentsResponse).tournaments;
-}
+  const url = `${apiHost}/tournaments`;
+  const resp = await getRequest(url, user);
+  return (resp as GetTournamentsResponse).tournaments;
+};
 
 // Get single tournmanet details
 export const getTournament = async (id: string, user: User) => {
-    const url = `${apiHost}/tournaments/${id}`;
-    console.log("GET: " , url);
-    const resp = await getRequest(url, user);
-    return resp as Tournament;
-}
+  const url = `${apiHost}/tournaments/${id}`;
+  console.log("GET: ", url);
+  const resp = await getRequest(url, user);
+  return resp as Tournament;
+};
 
-export const createTournament = async (user: User, tournamentData: Partial<Tournament>) => {
-    const url = `${apiHost}/tournaments`;
-    const resp = await postRequest(url, tournamentData, user);
-    return resp as Tournament;
-}
+export const createTournament = async (
+  user: User,
+  tournamentData: Partial<Tournament>
+) => {
+  const url = `${apiHost}/tournaments`;
+  const resp = await postRequest(url, tournamentData, user);
+  return resp as Tournament;
+};
 
 // Editing a specific tournament
-export const addBracket = async (bracket: Bracket, tournamentId: string, user: User) => {
-    const url = `${apiHost}/tournaments/${tournamentId}/brackets`;
-    const resp = await postRequest(url, bracket, user);
-    return resp as Bracket;
-}
+export const addBracket = async (
+  bracket: Bracket,
+  tournamentId: string,
+  user: User
+) => {
+  const url = `${apiHost}/tournaments/${tournamentId}/brackets`;
+  const resp = await postRequest(url, bracket, user);
+  return resp as Bracket;
+};
--- screens/TournamentDetails.tsx
+++ screens/TournamentDetails.tsx
@@ -10,91 +10,105 @@ import { AuthUserContext } from "../contexts/AuthContext";
 import { Tournament, Bracket } from "../models";
 import { ScreenWithNavigation } from "../types";
 
-type DetailScreen = ScreenWithNavigation & { route: Route<any> }
+type DetailScreen = ScreenWithNavigation & { route: Route<any> };
 
-export default function TournamentDetailsScreen({ navigation, route }: DetailScreen) {
+export default function TournamentDetailsScreen({
+  navigation,
+  route,
+}: DetailScreen) {
+  const { user } = useContext(AuthUserContext);
+  if (!user) {
+    throw new Error("User not defined");
+  }
+  const { id: tournamentId } = route.params as { id: string };
 
-    const { user } = useContext(AuthUserContext);
-    const { id: tournamentId } = route.params as { id: string };
+  // TODO: Update data and send to API when done
+  const [tournament, setTournament] = useState<Tournament | undefined>();
+  const [loading, setLoading] = useState(true);
 
-    // TODO: Update data and send to API when done
-    const [tournament, setTournament] = useState<Tournament | undefined>();
-    const [loading, setLoading] = useState(true);
+  const [bracket, setBracket] = useState<Bracket>({ name: "" });
 
-    const [bracket, setBracket] = useState<Bracket>({name: ''})
+  // For Adding brackets
+  const [showAddBracket, setShowAddBracketModal] = useState(false);
 
-    // For Adding brackets
-    const [showAddBracket, setShowAddBracketModal] = useState(false);
+  const fetchTournamentDetails = async () => {
+    const data = await getTournament(tournamentId, user);
+    setTournament(data);
+    setLoading(false);
+  };
 
-    const fetchTournamentDetails = async () => {
-        const data = await getTournament(tournamentId, user);
-        setTournament(data);
-        setLoading(false)
-    }
+  // On screen load, fetch the tournament details
+  useEffect(() => {
+    fetchTournamentDetails();
+  }, []);
 
-    // On screen load, fetch the tournament details
-    useEffect(() => {
-        fetchTournamentDetails();
-    }, []);
+  const submitAddBracket = async () => {
+    setLoading(true);
+    await addBracket(bracket, tournamentId, user);
+    setBracket({ name: "" });
+    // Refresh w/ new bracket
+    fetchTournamentDetails();
+  };
 
-    const submitAddBracket = async () => {
-        setLoading(true);
-        await addBracket(bracket, tournamentId, user);
-        setBracket({name: ''});
-        // Refresh w/ new bracket
-        fetchTournamentDetails();
-    }
+  if (loading || tournament === undefined) {
+    return <Spinner />;
+  }
 
-    if (loading || tournament === undefined) {
-        return (
-            <Spinner />
-        );
-    }
-
-    const renderAddBracketModal = () => {
-        return (
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={showAddBracket}
-                onRequestClose={() => {
-                    setShowAddBracketModal(!showAddBracket)
-                }}
-            >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <Text style={styles.modalText}>Add a Bracket to {tournament.name}</Text>
-                <TextInput placeholder="Bracket name" style={styles.textInput} onChangeText={(t) => setBracket({ ...bracket, name: t })}/>
-                <LinkButton title="Add" onPress={() => submitAddBracket()}/>
-                <LinkButton title="Cancel" onPress={() => setShowAddBracketModal(!showAddBracket)}/>
-              </View>
-            </View>
-          </Modal>  
-        );
-    }
-
+  const renderAddBracketModal = () => {
     return (
-        <View style={styles.container}>
-            {renderAddBracketModal()}
-            <Text style={styles.title}>{tournament?.name}</Text>
-
-            <Text style={styles.label}>Quick Join ID</Text>
-            <Text style={styles.value}>{tournament.shortId}</Text>
-
-            <Text style={styles.label}>Brackets</Text>
-            {
-                tournament.brackets?.map( b => {
-                    return (
-                        <View key={`bracket-${b.id}`}>
-                            <Text style={styles.value}>{b.name}</Text>
-                        </View>
-                    )
-                })
-            }
-
-            <LinkButton title="Add Bracket" onPress={() => setShowAddBracketModal(true)} style={{ marginTop: 24 }} />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showAddBracket}
+        onRequestClose={() => {
+          setShowAddBracketModal(!showAddBracket);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
+              Add a Bracket to {tournament.name}
+            </Text>
+            <TextInput
+              placeholder="Bracket name"
+              style={styles.textInput}
+              onChangeText={(t) => setBracket({ ...bracket, name: t })}
+            />
+            <LinkButton title="Add" onPress={() => submitAddBracket()} />
+            <LinkButton
+              title="Cancel"
+              onPress={() => setShowAddBracketModal(!showAddBracket)}
+            />
+          </View>
         </View>
+      </Modal>
     );
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderAddBracketModal()}
+      <Text style={styles.title}>{tournament?.name}</Text>
+
+      <Text style={styles.label}>Quick Join ID</Text>
+      <Text style={styles.value}>{tournament.shortId}</Text>
+
+      <Text style={styles.label}>Brackets</Text>
+      {tournament.brackets?.map((b) => {
+        return (
+          <View key={`bracket-${b.id}`}>
+            <Text style={styles.value}>{b.name}</Text>
+          </View>
+        );
+      })}
+
+      <LinkButton
+        title="Add Bracket"
+        onPress={() => setShowAddBracketModal(true)}
+        style={{ marginTop: 24 }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -103,17 +117,17 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   headerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 32,
-    fontWeight: '700',
+    fontWeight: "700",
     marginBottom: 20,
   },
   header: {
-      fontSize: 24,
-      fontWeight: '500',
+    fontSize: 24,
+    fontWeight: "500",
   },
   separator: {
     marginVertical: 30,
@@ -121,17 +135,17 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   tournamentListContainer: {
-    height: '40%',
+    height: "40%",
     margin: 20,
   },
   label: {
-      fontSize: 18,
-      fontWeight: '600',
-      marginTop: 20,
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 20,
   },
   value: {
     fontSize: 20,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   input: {
     fontSize: 24,
@@ -139,14 +153,14 @@ const styles = StyleSheet.create({
   },
   footer: {
     marginTop: 40,
-    flexDirection: 'row',
-    justifyContent: 'center'
+    flexDirection: "row",
+    justifyContent: "center",
   },
   centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    marginTop: 22,
   },
   modalView: {
     margin: 20,
@@ -156,17 +170,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
   },
   modalText: {
     marginBottom: 15,
-    textAlign: "center"
+    textAlign: "center",
   },
   textInput: {
-      fontSize: 20,
-  }
+    fontSize: 20,
+  },
 });
